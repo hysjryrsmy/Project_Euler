c60d484114cbb9f7db77710d233284c763233710
yiyi <1484491854a@gmail.com>
2021-06-03 17:31:52 +0900

    第6题

---
+++ project_euler_6.cpp
@@ -0,0 +1,17 @@
+﻿
+#define _CRT_SECURE_NO_WARING
+#include <iostream>
+using namespace std;
+
+//第6题
+
+int main() {
+	int psum = 0, sum = 0;
+	for (int i = 1; i <= 100; i++) {
+		psum += i * i;
+		sum += i;
+	}
+	cout << sum * sum - psum << endl;
+
+	return 0;
+}
